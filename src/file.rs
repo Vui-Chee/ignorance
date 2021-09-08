67a1e2370f38cdbd0da3d1226817321b43ea631a
Vui-Chee <vuicheesiew@gmail.com>
2021-09-08 16:03:53 +0800

    Temp allow deadcode to quash clippy warnings

--- src/file.rs
+++ src/file.rs
@@ -12,6 +12,7 @@ impl Storage {
     /// Returns a `Storage`.
     ///
     /// Also creates the directory where all templates will be stored.
+    #[allow(dead_code)]
     pub fn new(dirname: String) -> std::io::Result<Self> {
         if let Some(home) = home_dir() {
             let dirpath = home.join(&dirname);
@@ -22,6 +23,7 @@ impl Storage {
     }
 
     /// Returns the directory path where all the template files are stored.
+    #[allow(dead_code)]
     pub fn path(&self) -> Option<PathBuf> {
         if let Some(home) = home_dir() {
             return Some(home.join(&self.dirname));
@@ -31,11 +33,13 @@ impl Storage {
     }
 
     /// Returns template filename in the form of `<LANG>.gitignore`.
+    #[allow(dead_code)]
     pub fn template_filename(&self, lang: String) -> String {
         format!("{}.gitignore", lang)
     }
 
     /// Returns a contents of the template file (as String) for that language.
+    #[allow(dead_code)]
     pub fn get_template(&self, lang: String) -> std::io::Result<String> {
         let template_path = self.path().unwrap().join(self.template_filename(lang));
         let mut file = File::open(template_path)?;
@@ -49,6 +53,7 @@ impl Storage {
     ///
     /// Opens a new file is template file does not exist, otherwise
     /// overwrites existing template file.
+    #[allow(dead_code)]
     pub fn add_template(&self, lang: String, contents: &str) -> std::io::Result<()> {
         let template_path = self.path().unwrap().join(self.template_filename(lang));
         let mut file = File::create(template_path)?;
