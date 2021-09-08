320548ec5b96ecb93bf5ddd411a7c05a7908de37
Vui-Chee <vuicheesiew@gmail.com>
2021-09-08 15:36:39 +0800

    Storage struct for storing templates
    Serve as persistent store for target templates.

---
+++ src/file.rs
@@ -0,0 +1,59 @@
+use dirs::home_dir;
+use std::fs::{create_dir, File};
+use std::io::prelude::*;
+use std::path::PathBuf;
+
+#[derive(Debug, PartialEq)]
+pub struct Storage {
+    dirname: String,
+}
+
+impl Storage {
+    /// Returns a `Storage`.
+    ///
+    /// Also creates the directory where all templates will be stored.
+    pub fn new(dirname: String) -> std::io::Result<Self> {
+        if let Some(home) = home_dir() {
+            let dirpath = home.join(&dirname);
+            create_dir(dirpath)?;
+        }
+
+        Ok(Storage { dirname })
+    }
+
+    /// Returns the directory path where all the template files are stored.
+    pub fn path(&self) -> Option<PathBuf> {
+        if let Some(home) = home_dir() {
+            return Some(PathBuf::from(home.join(&self.dirname)));
+        }
+
+        None
+    }
+
+    /// Returns template filename in the form of `<LANG>.gitignore`.
+    pub fn template_filename(&self, lang: String) -> String {
+        format!("{}.gitignore", lang)
+    }
+
+    /// Returns a contents of the template file (as String) for that language.
+    pub fn get_template(&self, lang: String) -> std::io::Result<String> {
+        let template_path = self.path().unwrap().join(self.template_filename(lang));
+        let mut file = File::open(template_path)?;
+        let mut contents = String::new();
+        file.read_to_string(&mut contents)?;
+
+        Ok(contents)
+    }
+
+    /// Writes contents to a template file.
+    ///
+    /// Opens a new file is template file does not exist, otherwise
+    /// overwrites existing template file.
+    pub fn add_template(&self, lang: String, contents: &str) -> std::io::Result<()> {
+        let template_path = self.path().unwrap().join(self.template_filename(lang));
+        let mut file = File::create(template_path)?;
+        file.write(contents.as_bytes())?;
+
+        Ok(())
+    }
+}
---
+++ tests/storage.rs
@@ -0,0 +1,86 @@
+use dirs::home_dir;
+use std::fs::{create_dir, metadata, read_to_string, remove_dir, remove_dir_all, File};
+use std::io::prelude::*;
+use std::path::PathBuf;
+
+use ignorance::file::Storage;
+
+static STORAGE_DIR: &str = ".ignorance-test";
+
+fn create_storage_name(testname: &str) -> String {
+    format!("{}-{}", STORAGE_DIR, testname)
+}
+
+#[test]
+fn new_storage() -> std::io::Result<()> {
+    let test_storage_name = create_storage_name("new_storage");
+    let storage = Storage::new(test_storage_name.to_owned())?;
+
+    assert!(PathBuf::from(home_dir().unwrap().join(&test_storage_name)).exists());
+    assert!(metadata(storage.path().unwrap())?.is_dir());
+
+    remove_dir(home_dir().unwrap().join(test_storage_name))?;
+    Ok(())
+}
+
+#[test]
+fn directory_alrdy_exists() {
+    let test_storage_name = create_storage_name("directory_alrdy_exists");
+    let storage_path = home_dir().unwrap().join(&test_storage_name);
+    create_dir(&storage_path).unwrap();
+
+    assert!(Storage::new(test_storage_name).is_err());
+
+    remove_dir(storage_path).unwrap();
+}
+
+#[test]
+fn returns_path() -> std::io::Result<()> {
+    let test_storage_name = create_storage_name("returns_path");
+    let storage = Storage::new(test_storage_name.to_owned())?;
+
+    assert!(storage.path().is_some());
+    assert_eq!(
+        storage.path().unwrap(),
+        PathBuf::from(home_dir().unwrap().join(test_storage_name))
+    );
+
+    remove_dir(storage.path().unwrap())?;
+    Ok(())
+}
+
+#[test]
+fn get_template() -> std::io::Result<()> {
+    let test_storage_name = create_storage_name("get_template");
+    let storage = Storage::new(test_storage_name.to_owned())?;
+    let storage_path = home_dir().unwrap().join(test_storage_name);
+    let mut file = File::create(&storage_path.join("C++.gitignore"))?;
+    let contents = "# Prerequisites\n*.d\n# Compiled Object files\n*.slo\n*.lo\n*.o\n*.obj";
+    file.write(contents.as_bytes())?;
+    let template = storage.get_template("C++".to_owned())?;
+
+    assert_eq!(template, contents);
+
+    remove_dir_all(storage_path).unwrap();
+    Ok(())
+}
+
+#[test]
+fn add_template() -> std::io::Result<()> {
+    let test_storage_name = create_storage_name("add_template");
+    let storage_path = home_dir().unwrap().join(&test_storage_name);
+    let path_to_template = storage_path.join("C++.gitignore");
+
+    assert!(!path_to_template.exists());
+
+    let storage = Storage::new(test_storage_name)?;
+    let lang = "C++";
+    let contents = "# Prerequisites\n*.d\n# Compiled Object files\n*.slo\n*.lo\n*.o\n*.obj";
+    storage.add_template(lang.to_owned(), contents)?;
+
+    assert!(path_to_template.exists());
+    assert_eq!(read_to_string(path_to_template).unwrap(), contents);
+
+    remove_dir_all(storage_path).unwrap();
+    Ok(())
+}
