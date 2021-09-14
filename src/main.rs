861568df693fdc1a5dfa7d027fede768d30f12e8
Vui-Chee <vuicheesiew@gmail.com>
2021-09-14 18:12:04 +0800

    Update client to create gitignore using template

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,31 @@
+#[macro_use]
+extern crate lazy_static;
+
 mod file;
+mod language;
+mod request;
 mod url;
 
 use clap::{App, Arg};
 
-use std::fs::File;
-use std::io::prelude::*;
-use std::path::Path;
+use std::fs::copy;
+use std::process::exit;
 
-use url::create_url;
+use file::Storage;
+use request::fetch_template;
+use url::{template_dirpath, template_filepath};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let matches = App::new("ignorance")
-        .version("1.0")
+        .version(env!("CARGO_PKG_VERSION"))
         .author("Vui Chee <vuicheesiew@gmail.com>")
         .about("generates ignore files for you")
+        .arg(
+            Arg::with_name("update")
+                .short("u")
+                .help("Refetch template file from source"),
+        )
         .arg(
             Arg::with_name("lang")
                 .help("Create <LANGUAGE> .gitignore file.")
@@ -23,21 +34,35 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         )
         .get_matches();
 
+    let storage_dirpath = template_dirpath();
+    let storage = Storage::new(storage_dirpath.as_path()); // create template dir at home path.
+
     if let Some(lang) = matches.value_of("lang") {
-        let gitignore_url = create_url(lang.to_owned());
-        let client = reqwest::Client::new();
-        let res = client.get(gitignore_url).send().await?;
+        let filepath = template_filepath(lang);
 
-        let ignore_filepath = Path::new(".gitignore");
-        let mut file = match File::create(&ignore_filepath) {
-            Err(why) => panic!("couldn't create {}: {}", ignore_filepath.display(), why),
-            Ok(file) => file,
-        };
+        // Fetch from api if any of the two conditions are met:
+        // 1. force_update option is applied
+        // 2. template file does not exist
+        if matches.is_present("update") || !filepath.exists() {
+            let response = fetch_template(lang).await?;
 
-        let text = res.text().await?;
-        match file.write_all(text.as_bytes()) {
-            Err(why) => panic!("couldn't write to {}: {}", ignore_filepath.display(), why),
-            Ok(_) => println!("successfully wrote to {}", ignore_filepath.display()),
+            if response.status() >= reqwest::StatusCode::BAD_REQUEST {
+                eprintln!("Language Not Found");
+                exit(1);
+            }
+
+            let template = response.text().await?;
+            storage?.add_template(lang.to_owned(), &template)?;
+        }
+
+        // Otherwise, read contents from template filepath.
+        // NOTE: copy will create .gitignore if it does not exist.
+        match copy(filepath, ".gitignore") {
+            Ok(_) => {}
+            Err(_err) => {
+                eprintln!("Not a valid language");
+                exit(1);
+            }
         }
     }
 
