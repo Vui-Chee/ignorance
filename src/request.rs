2c9b597221946d578fd84f8a0410c1a636360d97
Vui-Chee <vuicheesiew@gmail.com>
2021-09-15 11:01:25 +0800

    Move create_url to request module

--- src/request.rs
+++ src/request.rs
@@ -1,4 +1,24 @@
-use crate::url::create_url;
+use std::path::Path;
+
+use crate::url::template_filename;
+
+pub fn create_url(lang: &str) -> String {
+    if lang.is_empty() {
+        panic!("lang should not be empty");
+    }
+
+    if !lang.is_ascii() {
+        panic!("lang should be ascii");
+    }
+
+    let domain = Path::new("https://raw.githubusercontent.com/github/gitignore/master/");
+
+    domain
+        .join(template_filename(lang).unwrap())
+        .to_str()
+        .unwrap()
+        .to_string()
+}
 
 pub async fn fetch_template(lang: &str) -> Result<reqwest::Response, reqwest::Error> {
     let gitignore_url = create_url(lang);
--- tests/url.rs
+++ tests/url.rs
@@ -1,7 +1,6 @@
 use dirs::home_dir;
 
-use ignorance::language::LANGUAGES_MAP;
-use ignorance::url::{create_url, template_filename, template_filepath};
+use ignorance::url::{template_filename, template_filepath};
 
 #[test]
 fn valid_template_filename() {
@@ -34,45 +33,4 @@ fn invalid_template_filepath() {
         template_filepath("asdf"),
         home_dir().unwrap().join(".ignorance")
     );
-}
-
-fn check_lang_url(lang: &str) {
-    let expected_domain = "https://raw.githubusercontent.com/github/gitignore/master/";
-    let gitignore_ext = ".gitignore";
-    let url = create_url(lang);
-    let url_len = url.len();
-    let lang_filename = LANGUAGES_MAP.get(lang).unwrap();
-
-    // check domain
-    assert_eq!(&url[..expected_domain.len()], expected_domain);
-    // check extension
-    assert_eq!(&url[url_len - gitignore_ext.len()..], gitignore_ext);
-    // check full url created
-    assert_eq!(
-        url,
-        format!(
-            "https://raw.githubusercontent.com/github/gitignore/master/{}.gitignore",
-            lang_filename
-        )
-    );
-}
-
-#[test]
-fn get_valid_url_for_lang() {
-    check_lang_url("c++");
-    check_lang_url("igorpro");
-    check_lang_url("craftcms");
-    check_lang_url("episerver");
-}
-
-#[test]
-#[should_panic(expected = "lang should not be empty")]
-fn empty_string() {
-    create_url("");
-}
-
-#[test]
-#[should_panic(expected = "lang should be ascii")]
-fn non_ascii() {
-    create_url(".网络");
 }
